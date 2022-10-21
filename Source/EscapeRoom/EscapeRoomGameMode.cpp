faeb0b276e213473f9fbfd66ba6ea5a2e456b6fd
Lina <107427736+Lina-cpp@users.noreply.github.com>
2022-10-21 12:29:02 +0200

    init

--- Source/EscapeRoom/EscapeRoomGameMode.cpp
+++ Source/EscapeRoom/EscapeRoomGameMode.cpp
@@ -8,7 +8,8 @@ AEscapeRoomGameMode::AEscapeRoomGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
+	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/MyStuff/Character/BP_FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 }
